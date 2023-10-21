3db96c2f1f12598666702baaa83318bb3d82327a
Birkity <85987355+Birkity@users.noreply.github.com>
2023-10-21 18:10:59 +0300

    Add a product model class

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductManagementSystem.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public bool IsAvailable => QuantityInStock > 0;
+    }
+
+}
